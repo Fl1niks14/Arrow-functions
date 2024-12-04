52a71a7dfae39ef43425ea56184a6bbac0ea806e
sphereFl1niks <166011502+Fl1niks14@users.noreply.github.com>
2024-12-04 12:54:28 +0400

    Arrow functions

---
+++ index.js
@@ -0,0 +1,14 @@
+
+/* 1 */
+
+const average = (num1, num2) => (num1 + num2)
+console.log(average(1, 1));
+
+/* 2 */
+
+const sayHello = () => console.log("Привет, мир!");
+
+/* 3 */
+
+const Name = (name) => console.log(`Привет, ${name}!`);
+Name("Андрей");
